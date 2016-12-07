cdd651e8439b804106247188fc0c86a2f72b8890
davips <davips@educ-312151.educ.guarulhos.gov>
2016-12-07 15:38:18 -0200

    Subindo notícias

--- header.php
+++ header.php
@@ -11,17 +11,18 @@
         <div class="container">
             <div class="row header">
                 <div class="col-sm-4 logo">
-                    <h1><a href="index.html">JMJ 2019<br />Guarulhos Panam&aacute;</a> <span>.</span></h1>
+                    <h1><a href="index.html">JMJ 2019<br />Guarulhos Panam&aacute;</a></h1>
                 </div>
                 <div class="col-sm-8 call-us">
                     <span><a href="http://guarulhosjmj2019.esy.es">In&iacute;cio | </a></span>
-                    <span><a href="#" onclick="alert('Pagina em desenvolvimento')">Not&iacute;cias | </a></span>
+                    <span><a href="http://guarulhosjmj2019.esy.es/noticias.php">Not&iacute;cias | </a></span>
                     <span><a href="#" onclick="alert('Pagina em desenvolvimento')">Testemunhos | </a></span>
                     <span><a href="#" onclick="alert('Pagina em desenvolvimento')">Fotos | </a></span>
-                    <span><a href="http://guarulhosjmj2019.esy.es/formularios/inscricao.php">Participe</a></span><br/>
+                    <span><a href="http://guarulhosjmj2019.esy.es/formularios/inscricao.php">Participe</a></span>
                 </div>
             </div>
         </div>
+        <hr style="height: 12px; border: 0; box-shadow: inset 0 5px 5px -5px rgba(0, 0, 0, 0.5);">
 
     </body>
 
